06c73f92d5a2cc50a8dbb2e48d23552a6502fd4c
iljakalistratov <71067944+iljakalistratov@users.noreply.github.com>
2022-04-28 09:49:09 +0200

    Initialized Django Project

---
+++ src/OnlineShop/databaseController/mainController.py
@@ -0,0 +1,5 @@
+import psycopg2 as pg2
+
+conn = pg2.connect(database="postgres", user="postgres", password="admin", host="localhost", port="5432")
+cur = conn.cursor()
+cur.execute("SELECT * FROM public.Orders")
