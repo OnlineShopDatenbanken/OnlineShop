c478772e5c193e08a8034dbe32ff5e79dc537359
Frederik <inf20092@lehre.dhbw-stuttgart.de>
2022-05-12 23:37:21 +0200

    Product page is almost finished. Displaying star rating in stars instead of numbers is to be done

--- src/static/products.js
+++ src/static/products.js
@@ -37,10 +37,42 @@ function showAvgProductRating(avgProductRatings)
     let sum = 0;
     for (let i = 0; i < avgProductRatings.length; i++)
     {
-        console.log(sum);
+        sum += avgProductRatings[i].avg;
     }
     result = (sum / avgProductRatings.length).toFixed(2);
 
     avgProductRatingDiv = document.getElementById("avgProductRating");
     avgProductRatingDiv.textContent += result;
+}
+
+function populateAvgProductRatingsList(avgProductRatings)
+{
+    table = document.getElementById("avgProductRatings")
+    trHeader = document.createElement("tr")
+    thTitle = document.createElement("th")
+    thTitle.textContent = "Product Name"
+    thTitle.classList.add("avgTableTitle");
+    thAvg = document.createElement("th")
+    thAvg.textContent = "Average Rating"
+    thAvg.classList.add("avgTableValue");
+
+    trHeader.appendChild(thTitle)
+    trHeader.appendChild(thAvg)
+
+    table.appendChild(trHeader)
+    for (let i = 0; i < avgProductRatings.length; i++)
+    {
+        trCurr = document.createElement("tr")
+        thCurrTitle = document.createElement("th")
+        thCurrTitle.textContent = avgProductRatings[i].title
+        thCurrTitle.classList.add("avgTableTitle");
+        thCurrAvg = document.createElement("th")
+        thCurrAvg.textContent = avgProductRatings[i].avg
+        thCurrAvg.classList.add("avgTableValue");
+
+        trCurr.appendChild(thCurrTitle)
+        trCurr.appendChild(thCurrAvg)
+        table.appendChild(trCurr)
+    }
+
 }
