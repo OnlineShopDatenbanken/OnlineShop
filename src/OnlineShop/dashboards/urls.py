a3cdc1fa523f0945a85a65ec8f4cd3f8e76a5f46
Frederik <inf20092@lehre.dhbw-stuttgart.de>
2022-05-14 21:19:16 +0200

    Add navigation between sites

--- src/OnlineShop/dashboards/urls.py
+++ src/OnlineShop/dashboards/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('customers', views.customers, name='customers'),
     path('products', views.products, name='products'),
     path('orders', views.orders, name='orders')
